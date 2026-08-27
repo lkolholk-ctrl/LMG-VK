a34fd8b23c4e009c48fb86f0dada86861f5c9414
root <root@gsgit.org>
2026-08-27 13:57:38 +0000

    Fix Apple TTML proxy connection

--- app/src/main/kotlin/com/lmg/vk/engine/lyrics/apple/AppleLyricsConfig.kt
+++ app/src/main/kotlin/com/lmg/vk/engine/lyrics/apple/AppleLyricsConfig.kt
@@ -2,5 +2,5 @@ package com.lmg.vk.engine.lyrics.apple
 
 /** Single application-side endpoint for the self-hosted raw Apple TTML proxy. */
 object AppleLyricsConfig {
-    const val PROXY_BASE_URL = "https://lyrics.gsgit.org"
+    const val PROXY_BASE_URL = "http://lyrics.gsgit.org"
 }
